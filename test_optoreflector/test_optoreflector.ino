9cd3f6c711bf3ba12edc1a4a108fafdfbc4d32a7
Faizaan Pervaiz <fp361@cam.ac.uk>
2020-01-26 19:12:34 +0000

    Updated for 3 optoreflectors

--- test_optoreflector/test_optoreflector.ino
+++ test_optoreflector/test_optoreflector.ino
@@ -1,13 +1,22 @@
-int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
-                    // outside leads to ground and +5V
-int val = 0;  // variable to store the value read
+int in1 = A0;
+int in2 = A1;
+int in3 = A2;
+
+int v1 = 0;
+int v2 = 0;
+int v3 = 0;
 
 void setup() {
-  Serial.begin(9600);           //  setup serial
+  Serial.begin(9600); 
 }
 
 void loop() {
-  val = analogRead(analogPin);  // read the input pin
-  Serial.println(val);          // debug value
-  delay(100);
+  v1 = analogRead(in1);
+  v2 = analogRead(in2);
+  v3 = analogRead(in3);
+  Serial.println(v1);
+  Serial.println(v2);
+  Serial.println(v3);
+  Serial.println(" ");
+  delay(250);
 }

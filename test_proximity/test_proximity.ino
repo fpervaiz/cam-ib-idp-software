9a03a2cc683f771c2c11087db69f2ac7c23ff794
Puria Radmard <pr450@cam.ac.uk>
2020-01-27 12:10:55 +0000

    tried proximity sensor

--- test_proximity/test_proximity.ino
+++ test_proximity/test_proximity.ino
@@ -1,19 +1,29 @@
 /*SHARP GP2Y0A21YK0F IR sensor with Arduino and SharpIR library example code. More info: https://www.makerguides.com */
-// Include the library:
+// Include the library(-ies):
 #include <SharpIR.h>
+#include <RingBuffer.h>
+
 // Define model and input pin:
 #define IRPin A7
 #define model 1080
-// Create variable to store the distance:
+
+// Create variable to store the distances:
 double analogread;
 double voltage;
-double distance;
+double dis;
+double avg_dis;
+
+using namespace std;
+
+RingBuffer<double, 6> dis_his;
+
 /* Model :
   GP2Y0A02YK0F --> 20150
   GP2Y0A21YK0F --> 1080
   GP2Y0A710K0F --> 100500
   GP2YA41SK0F --> 430
 */
+
 
 // Create a new instance of the SharpIR class:
 //SharpIR mySensor = SharpIR(IRPin, model);
@@ -25,9 +35,16 @@ void setup() {
 void loop() {
   analogread = analogRead(IRPin);
   voltage = analogread*5/1024;
-  distance = 27.726*pow(voltage, -1.2045);
-  Serial.print("Mean distance: ");
-  Serial.print(voltage);
-  Serial.println(" cm");
+  dis = 27.726*pow(voltage, -1.2045);
+  dis_his.push_back(dis);
+  
+  //Serial.println("Mean distance: %d cm", 1dis);
   delay(1000);
+
+  avg_dis = 0.0;
+  for (int i = 0; i < dis_his.size(); i++){
+      avg_dis += dis_his[i];
+    }
+  avg_dis /= dis_his.size();
+  Serial.println(avg_dis);
 }
